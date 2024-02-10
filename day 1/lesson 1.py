43b58e22947eba28373b2041720811942c164e33
merabiko2710 <mindia.gaganidze@yahoo.com>
2024-02-10 18:06:16 +0400

    second commit

---
+++ day 1/lesson 1.py
@@ -0,0 +1,10 @@
+name = "merabi"
+print(name)
+surname = "gaganidze"
+print(name + " " + surname)
+age = "9"
+print(name +" " + str(age))
+print(name + " " + surname +" " + str(age))
+hight = "1,50"
+weght = "45"
+print(name + " " + surname + " " + str(age) + " " + hight + " " +  weght)
