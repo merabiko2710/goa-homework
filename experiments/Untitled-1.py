f5c3b3940fa4bf9e582cf240af65a9b4229a6f32
merabiko2710 <mindia.gaganidze@yahoo.com>
2024-03-24 17:37:22 +0400

    second commit

--- experiments/Untitled-1.py
+++ experiments/Untitled-1.py
@@ -1,5 +1,19 @@
 age = int (input ("how old are you ?:"))
 brother_age = int (input ("how old is your broter ?:"))
 print (age + brother_age + (50 *2) )
-type (7)
-object = all
+GAME_WIDTH = 700
+GAME_HIGHT = 700
+SPEED = 35
+import random
+class Snake:
+pass
+class Food:
+pass
+def next_turn():
+    pass
+def change_derection(new direction):
+    pass
+def check_collisions():
+    pass
+def game_over():
+    pass
